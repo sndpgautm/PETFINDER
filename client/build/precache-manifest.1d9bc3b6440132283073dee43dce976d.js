08330c60b6bbf3e0d1eb78476521948ba144b657
Sandip Gautam <rockersandip96@gmail.com>
2020-06-05 17:57:40 +0300

    ready for production

---
+++ client/build/precache-manifest.1d9bc3b6440132283073dee43dce976d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a3f59975fb0adebee71bf9a876e6793",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55fefa60e94722644560",
+    "url": "/static/css/main.59e803e4.chunk.css"
+  },
+  {
+    "revision": "31c8e9eaebc1c0728d0c",
+    "url": "/static/js/2.95cc4cbd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.95cc4cbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55fefa60e94722644560",
+    "url": "/static/js/main.ae79bf80.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "288e09632ca2a144c694d98e3365ab63",
+    "url": "/static/media/dog-hero.288e0963.jpg"
+  }
+]);
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 // Connect Database
@@ -8,15 +8,32 @@ connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
-app.get('/', (req, res) => res.send('API running'));
 
-// Static Folder
-app.use(express.static('./client/public'));
+//For jelastic ssl use
+app.enable('trust proxy');
+
+//For local dev disable https conenction
+//Only enable when delpolying to git and jelastic
+app.use((req, res, next) => {
+  if (req.secure) {
+    // request was via https, so do no special handling
+    next();
+  } else {
+    // request was via http, so redirect to https
+    res.redirect('https://' + req.headers.host + req.url);
+  }
+});
 
 // Define Routes
 app.use('/api/organizations', require('./routes/api/organizations'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/animals', require('./routes/api/animals'));
+
+// Serve Static assets in production
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
