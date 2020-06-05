53350fa320fd5b4753e18ddd524d290d90908318
Sandip Gautam <rockersandip96@gmail.com>
2020-06-05 20:23:39 +0300

    Revert "update"
    This reverts commit 4d99e0dc89204d27fe8ad006105eebd37b3e07ec.

--- server.js
+++ server.js
@@ -33,13 +33,10 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/animals', require('./routes/api/animals'));
 
 // Serve Static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set Static Folder
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
