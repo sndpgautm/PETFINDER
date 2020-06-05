4d99e0dc89204d27fe8ad006105eebd37b3e07ec
Sandip Gautam <rockersandip96@gmail.com>
2020-06-05 19:18:40 +0300

    update

--- server.js
+++ server.js
@@ -33,10 +33,13 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/animals', require('./routes/api/animals'));
 
 // Serve Static assets in production
-app.use(express.static('client/build'));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  // Set Static Folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
