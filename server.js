6260cca9973c393335e7cc8e6afb1c44104e1307
Sandip Gautam <rockersandip96@gmail.com>
2020-06-05 18:54:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 });
 
 // Static Folder
-app.use(express.static('./client/public'));
+//app.use(express.static('./client/public'));
 
 // Define Routes
 app.use('/api/organizations', require('./routes/api/organizations'));
