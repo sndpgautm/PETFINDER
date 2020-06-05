2740efcd1230302a64c9e92ae7aee977e7feb2ea
Sandip Gautam <rockersandip96@gmail.com>
2020-06-05 20:17:59 +0300

    Revert "updated server,js and new build"
    This reverts commit 0a3e0f7a1a11c25b36dde02d3732f6e386a145a1.

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ const app = express();
 // Connect Database
 connectDB();
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
 // Init Middleware
 app.use(express.json({ extended: false }));
 
@@ -41,3 +38,7 @@ app.use(express.static('client/build'));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
